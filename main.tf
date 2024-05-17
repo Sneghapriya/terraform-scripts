0ad7eaa871fa39eb35d613637e6599109ea5d280
Sneghapriya <102730861+Sneghapriya@users.noreply.github.com>
2024-05-17 22:43:46 +0530

    update

--- main.tf
+++ main.tf
@@ -1,51 +1,48 @@
+
 terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 4.0.0"
+      version = "~> 4.0"
     }
   }
 }
 
 resource "google_container_cluster" "primary" {
-  name     = "test_cluster"
+  name     = "test-cluster"
   location = "us-central1-a"
- project  = ""
+ project  = "lumen-b-ctl-047"
+
+  remove_default_node_pool = true
+  initial_node_count       = 1
 
   networking_mode = "VPC_NATIVE"
-  ip_allocation_policy {
-  }
-  remove_default_node_pool = true
 
-  release_channel {
-    channel = "REGULAR"
+  workload_identity_config {
+    workload_pool = "lumen-b-ctl-047.svc.id.goog"
   }
 }
 
-resource "google_container_node_pool" "primary_nodes" {
-  name       = "primary-node-pool"
+resource "google_container_node_pool" "pool_1" {
+  name       = "pool-1"
   location  = "us-central1-a"
-  project   = ""
+  project   = "lumen-b-ctl-047"
   cluster   = google_container_cluster.primary.name
   node_count = 1
 
-  autoscaling {}
-
-  management {
-    auto_repair  = true
-    auto_upgrade = true
+  autoscaling {
+    enabled = false
   }
+}
 
-  node_config {
-    machine_type = "e2-medium"
-    disk_size_gb = 100
-    disk_type    = "pd-standard"
+resource "google_container_node_pool" "pool_2" {
+  name       = "pool-2"
+  location  = "us-central1-a"
+  project   = "lumen-b-ctl-047"
+  cluster   = google_container_cluster.primary.name
+  node_count = 1
 
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/compute",
-      "https://www.googleapis.com/auth/devstorage.read_only",
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-    ]
+  autoscaling {
+    enabled = false
   }
 }
