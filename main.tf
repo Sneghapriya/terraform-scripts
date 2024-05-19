32ee96b67b0bc83c23ef0ad35c46b0302dc69275
Sneghapriya <102730861+Sneghapriya@users.noreply.github.com>
2024-05-19 23:42:56 +0530

    update

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 terraform {
   required_providers {
     google = {
-      source  = "hashicorp/google"
+      source = "hashicorp/google"
       version = "~> 4.0"
     }
   }
@@ -10,19 +10,22 @@ terraform {
 
 resource "google_container_cluster" "primary" {
   name     = "test-cluster"
-  location = "us-central1"
+  location = "us-central1-a"
  project  = "sampleproject"
 
   remove_default_node_pool = true
   initial_node_count       = 1
 
-  networking_mode = "k8s_service"
+  networking_mode = "VPC_NATIVE"
 
+  workload_identity_config {
+    workload_pool = "${google_project.project.project_id}.svc.id.goog"
+  }
 }
 
-resource "google_container_node_pool" "pool1" {
+resource "google_container_node_pool" "pool_1" {
   name       = "pool-1"
-  location  = "us-central1"
+  location  = "us-central1-a"
   project   = "sampleproject"
   cluster   = google_container_cluster.primary.name
 
@@ -30,12 +33,16 @@ resource "google_container_node_pool" "pool1" {
 
   autoscaling {
     enabled = false
+  }
+
+  management {
+    auto_repair = true
   }
 }
 
-resource "google_container_node_pool" "pool2" {
+resource "google_container_node_pool" "pool_2" {
   name       = "pool-2"
-  location  = "us-central1"
+  location  = "us-central1-a"
   project   = "sampleproject"
   cluster   = google_container_cluster.primary.name
 
@@ -44,4 +51,12 @@ resource "google_container_node_pool" "pool2" {
   autoscaling {
     enabled = false
   }
+
+  management {
+    auto_repair = true
+  }
+}
+
+resource "google_project" "project" {
+  project_id = "sampleproject"
 }
