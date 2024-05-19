f1365b51be38b917d78e5d0be611608600f7acde
Sneghapriya <102730861+Sneghapriya@users.noreply.github.com>
2024-05-19 23:04:23 +0530

    update

--- main.tf
+++ main.tf
@@ -11,14 +11,15 @@ terraform {
 resource "google_container_cluster" "primary" {
   name     = "test-cluster"
   location = "us-central1-a"
-  project  = "sampleproject"
+ project  = "sampleproject"
 
   remove_default_node_pool = true
   initial_node_count       = 1
 
-  networking_mode = "k8s_pod_networking"
+  networking_mode = "k8s_service"
 
-  ip_allocation_policy {
+  workload_identity_config {
+ workload_pool = "sampleproject.svc.id.goog"
   }
 }
 
@@ -29,9 +30,8 @@ resource "google_container_node_pool" "pool1" {
   cluster   = google_container_cluster.primary.name
   node_count = 1
 
-  autoscaling {
-    disabled = true
-  }
+  autoscaling {}
+
   management {
     auto_repair  = true
     auto_upgrade = true
@@ -42,7 +42,7 @@ resource "google_container_node_pool" "pool1" {
     disk_size_gb = 100
     oauth_scopes = [
       "https://www.googleapis.com/auth/compute",
-      "https://www.googleapis.com/auth/devstorage.read_write",
+      "https://www.googleapis.com/auth/devstorage.read_only",
       "https://www.googleapis.com/auth/logging.write",
       "https://www.googleapis.com/auth/monitoring",
     ]
@@ -56,9 +56,8 @@ resource "google_container_node_pool" "pool2" {
   cluster   = google_container_cluster.primary.name
   node_count = 1
 
-  autoscaling {
-    disabled = true
-  }
+  autoscaling {}
+
   management {
     auto_repair  = true
     auto_upgrade = true
@@ -69,7 +68,7 @@ resource "google_container_node_pool" "pool2" {
     disk_size_gb = 100
     oauth_scopes = [
       "https://www.googleapis.com/auth/compute",
-      "https://www.googleapis.com/auth/devstorage.read_write",
+      "https://www.googleapis.com/auth/devstorage.read_only",
       "https://www.googleapis.com/auth/logging.write",
       "https://www.googleapis.com/auth/monitoring",
     ]
