1c3f8c4072ccebf7bdb5f940f9e729489122d5e0
Sneghapriya <102730861+Sneghapriya@users.noreply.github.com>
2024-05-19 21:33:04 +0530

    update

--- main.tf
+++ main.tf
@@ -10,13 +10,13 @@ terraform {
 
 resource "google_container_cluster" "primary" {
   name     = "test-cluster"
-  location = "us-central1"
- project  = "sampleproject"
+  location = "us-central1-a"
+  project  = "sampleproject"
 
   remove_default_node_pool = true
   initial_node_count       = 1
 
-  networking_mode = "VPC_NATIVE"
+  networking_mode = "k8s_pod_networking"
 
   workload_identity_config {
     workload_pool = "sampleproject.svc.id.goog"
@@ -25,14 +25,13 @@ resource "google_container_cluster" "primary" {
 
 resource "google_container_node_pool" "pool1" {
   name       = "pool-1"
-  location  = "us-central1"
+  location  = "us-central1-a"
   project   = "sampleproject"
   cluster   = google_container_cluster.primary.name
   node_count = 1
 
-  autoscaling {
-    enabled = false
-  }
+  autoscaling {}
+
   management {
     auto_repair  = true
     auto_upgrade = true
@@ -52,14 +51,13 @@ resource "google_container_node_pool" "pool1" {
 
 resource "google_container_node_pool" "pool2" {
   name       = "pool-2"
-  location  = "us-central1"
+  location  = "us-central1-a"
   project   = "sampleproject"
   cluster   = google_container_cluster.primary.name
   node_count = 1
 
-  autoscaling {
-    enabled = false
-  }
+  autoscaling {}
+
   management {
     auto_repair  = true
     auto_upgrade = true
