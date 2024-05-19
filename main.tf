83d27ea85cae47f8c4e38f9f21cc7d85426e944f
Sneghapriya <102730861+Sneghapriya@users.noreply.github.com>
2024-05-19 23:04:43 +0530

    update

--- main.tf
+++ main.tf
@@ -10,67 +10,44 @@ terraform {
 
 resource "google_container_cluster" "primary" {
   name     = "test-cluster"
-  location = "us-central1-a"
+  location = "us-central1"
  project  = "sampleproject"
 
   remove_default_node_pool = true
   initial_node_count       = 1
 
-  networking_mode = "k8s_service"
+  networking_mode = "k8s_ipo_alloc"
 
-  workload_identity_config {
- workload_pool = "sampleproject.svc.id.goog"
+  ip_allocation_policy {
   }
 }
 
 resource "google_container_node_pool" "pool1" {
   name       = "pool-1"
-  location  = "us-central1-a"
+  location  = "us-central1"
   project   = "sampleproject"
   cluster   = google_container_cluster.primary.name
-  node_count = 1
-
-  autoscaling {}
-
-  management {
-    auto_repair  = true
-    auto_upgrade = true
-  }
 
   node_config {
     machine_type = "n1-standard-1"
-    disk_size_gb = 100
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/compute",
-      "https://www.googleapis.com/auth/devstorage.read_only",
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-    ]
+  }
+
+  autoscaling {
+    enabled = false
   }
 }
 
 resource "google_container_node_pool" "pool2" {
   name       = "pool-2"
-  location  = "us-central1-a"
+  location  = "us-central1"
   project   = "sampleproject"
   cluster   = google_container_cluster.primary.name
-  node_count = 1
-
-  autoscaling {}
-
-  management {
-    auto_repair  = true
-    auto_upgrade = true
-  }
 
   node_config {
-    machine_type = "n1-standard-1"
-    disk_size_gb = 100
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/compute",
-      "https://www.googleapis.com/auth/devstorage.read_only",
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-    ]
+    machine_type = "n1-standard-2"
+  }
+
+  autoscaling {
+    enabled = false
   }
 }
