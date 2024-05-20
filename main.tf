274fd92f29ce72f375150ac043868daf55617208
Sneghapriya <102730861+Sneghapriya@users.noreply.github.com>
2024-05-20 12:30:59 +0530

    update

--- main.tf
+++ main.tf
@@ -11,67 +11,39 @@ terraform {
 resource "google_container_cluster" "primary" {
   name     = "test-cluster"
   location = "us-central1"
-  project  = "sampleproject"
+ project  = "sampleproject"
 
   remove_default_node_pool = true
   initial_node_count       = 1
 
-  networking_mode = "k8s_ipo_alloc"
+  networking_mode = "k8s_service"
 
-  ip_allocation_policy {
-  }
 }
 
 resource "google_container_node_pool" "pool1" {
   name       = "pool-1"
+  cluster    = google_container_cluster.primary.name
   location  = "us-central1"
-  project   = "sampleproject"
-  cluster   = google_container_cluster.primary.name
-  node_count = 1
-
-  autoscaling {
-    enabled = false
-  }
-  management {
-    auto_repair  = true
-    auto_upgrade = true
-  }
+ project   = "sampleproject"
 
   node_config {
     machine_type = "n1-standard-1"
-    disk_size_gb = 100
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/compute",
-      "https://www.googleapis.com/auth/devstorage.read_only",
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-    ]
+  }
+  autoscaling {
+    enabled = false
   }
 }
 
 resource "google_container_node_pool" "pool2" {
   name       = "pool-2"
+  cluster    = google_container_cluster.primary.name
   location  = "us-central1"
-  project   = "sampleproject"
-  cluster   = google_container_cluster.primary.name
-  node_count = 1
-
-  autoscaling {
-    enabled = false
-  }
-  management {
-    auto_repair  = true
-    auto_upgrade = true
-  }
+ project   = "sampleproject"
 
   node_config {
-    machine_type = "n1-standard-1"
-    disk_size_gb = 100
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/compute",
-      "https://www.googleapis.com/auth/devstorage.read_only",
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-    ]
+    machine_type = "n1-standard-2"
+  }
+  autoscaling {
+    enabled = false
   }
 }
