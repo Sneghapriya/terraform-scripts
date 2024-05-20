d90eb470e7889077cd72c1e6451153164ee23372
Sneghapriya <102730861+Sneghapriya@users.noreply.github.com>
2024-05-20 21:36:24 +0530

    update

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ resource "google_container_cluster" "primary" {
   remove_default_node_pool = true
   initial_node_count       = 1
 
-  networking_mode = "VPC_NATIVE"
+  networking_mode = "k8s_ipo_alloc"
 
   ip_allocation_policy {
   }
@@ -28,18 +28,12 @@ resource "google_container_node_pool" "pool1" {
   project   = "sampleproject"
   cluster   = google_container_cluster.primary.name
 
-  node_count = 1
-
-  autoscaling {}
-
-  management {
-    auto_repair  = true
-    auto_upgrade = true
-  }
-
   node_config {
     machine_type = "n1-standard-1"
-    disk_size_gb = 100
+  }
+
+  autoscaling {
+    enabled = false
   }
 }
 
@@ -49,17 +43,11 @@ resource "google_container_node_pool" "pool2" {
   project   = "sampleproject"
   cluster   = google_container_cluster.primary.name
 
-  node_count = 1
-
-  autoscaling {}
-
-  management {
-    auto_repair  = true
-    auto_upgrade = true
+  node_config {
+    machine_type = "n1-standard-2"
   }
 
-  node_config {
-    machine_type = "n1-standard-1"
-    disk_size_gb = 100
+  autoscaling {
+    enabled = false
   }
 }
