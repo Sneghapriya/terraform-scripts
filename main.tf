f571421d943acd9767df954875b1d4ba4f3fad7c
Sneghapriya <102730861+Sneghapriya@users.noreply.github.com>
2024-05-20 16:04:51 +0530

    update

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ terraform {
 
 resource "google_container_cluster" "primary" {
   name     = "test-cluster"
-  location = "us-central1"
+  location = "us-central1-a"
  project  = "sampleproject"
 
   remove_default_node_pool = true
@@ -18,36 +18,57 @@ resource "google_container_cluster" "primary" {
 
   networking_mode = "VPC_NATIVE"
 
-  ip_allocation_policy {
+  workload_identity_config {
+ workload_pool = "sampleproject.svc.id.goog"
   }
 }
 
 resource "google_container_node_pool" "pool1" {
   name       = "pool-1"
-  location  = "us-central1"
+  location  = "us-central1-a"
   project   = "sampleproject"
   cluster   = google_container_cluster.primary.name
+  node_count = 1
+
+  autoscaling {}
+  management {
+    auto_repair  = true
+    auto_upgrade = true
+  }
 
   node_config {
     machine_type = "n1-standard-1"
-  }
-
-  autoscaling {
-    enabled = false
+    disk_size_gb = 100
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/compute",
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
   }
 }
 
 resource "google_container_node_pool" "pool2" {
   name       = "pool-2"
-  location  = "us-central1"
+  location  = "us-central1-a"
   project   = "sampleproject"
   cluster   = google_container_cluster.primary.name
+  node_count = 1
 
-  node_config {
-    machine_type = "n1-standard-2"
+  autoscaling {}
+  management {
+    auto_repair  = true
+    auto_upgrade = true
   }
 
-  autoscaling {
-    enabled = false
+  node_config {
+    machine_type = "n1-standard-1"
+    disk_size_gb = 100
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/compute",
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
   }
 }
