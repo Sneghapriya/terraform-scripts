31247ba2d6494db02c7f36cb42f6f2f69eaee3b4
Sneghapriya <102730861+Sneghapriya@users.noreply.github.com>
2024-05-20 22:03:48 +0530

    update

--- main.tf
+++ main.tf
@@ -10,36 +10,72 @@ terraform {
 
 resource "google_container_cluster" "primary" {
   name     = "test-cluster"
-  location = "us-central1"
+  location = "us-central1-a"
  project  = "sampleproject"
 
-  remove_default_node_pool = true
-  initial_node_count       = 1
-
   networking_mode = "VPC_NATIVE"
+  initial_node_count = 1
 
-}
+  ip_allocation_policy {
+  }
 
-resource "google_container_node_pool" "pool1" {
-  name       = "pool-1"
-  location  = "us-central1"
-  project   = "sampleproject"
-  cluster   = google_container_cluster.primary.name
-  node_count = 1
+  remove_default_node_pool = true
 
-  autoscaling {
-    enabled = false
+  release_channel {
+    channel = "REGULAR"
   }
 }
 
-resource "google_container_node_pool" "pool2" {
-  name       = "pool-2"
-  location  = "us-central1"
+resource "google_container_node_pool" "primary_nodes" {
+  name       = "primary-node-pool"
+  location  = "us-central1-a"
   project   = "sampleproject"
   cluster   = google_container_cluster.primary.name
+
   node_count = 1
 
-  autoscaling {
-    enabled = false
+  management {
+    auto_repair  = true
+    auto_upgrade = true
+  }
+
+  node_config {
+    machine_type = "n1-standard-1"
+    disk_size_gb = 100
+    disk_type    = "pd-standard"
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/compute",
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+  }
+}
+
+resource "google_container_node_pool" "secondary_nodes" {
+  name       = "secondary-node-pool"
+  location  = "us-central1-a"
+  project   = "sampleproject"
+  cluster   = google_container_cluster.primary.name
+
+  node_count = 1
+
+  management {
+    auto_repair  = true
+    auto_upgrade = true
+  }
+
+  node_config {
+    machine_type = "n1-standard-1"
+    disk_size_gb = 100
+    disk_type    = "pd-standard"
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/compute",
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
   }
 }
