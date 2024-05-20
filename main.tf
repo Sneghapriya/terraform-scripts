3a2d17eb448d4c5465440953612e0621ad3e7c35
Sneghapriya <102730861+Sneghapriya@users.noreply.github.com>
2024-05-20 23:48:14 +0530

    update

--- main.tf
+++ main.tf
@@ -10,71 +10,26 @@ terraform {
 
 resource "google_container_cluster" "primary" {
   name     = "test-cluster"
-  location = "us-central1-a"
+  location = "us-central1"
  project  = "sampleproject"
 
-  networking_mode = "VPC_NATIVE"
-  ip_allocation_policy {
+  node_pool {
+    name       = "default-pool"
+    node_count = 1
+
+    autoscaling {
+      enabled = false
+    }
   }
+
+  node_pool {
+    name       = "secondary-pool"
+    node_count = 1
+
+    autoscaling {
+      enabled = false
+    }
+  }
+
   remove_default_node_pool = true
-
-  release_channel {
-    channel = "REGULAR"
-  }
-}
-
-resource "google_container_node_pool" "pool1" {
-  name       = "pool-1"
-  location  = "us-central1-a"
-  project   = "sampleproject"
-  cluster   = google_container_cluster.primary.name
-  node_count = 1
-
-  autoscaling {
-    enabled = false
-  }
-
-  management {
-    auto_repair  = true
-    auto_upgrade = true
-  }
-
-  node_config {
-    machine_type = "n1-standard-1"
-    disk_size_gb = 100
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/compute",
-      "https://www.googleapis.com/auth/devstorage.read_only",
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-    ]
-  }
-}
-
-resource "google_container_node_pool" "pool2" {
-  name       = "pool-2"
-  location  = "us-central1-a"
-  project   = "sampleproject"
-  cluster   = google_container_cluster.primary.name
-  node_count = 1
-
-  autoscaling {
-    enabled = false
-  }
-
-  management {
-    auto_repair  = true
-    auto_upgrade = true
-  }
-
-  node_config {
-    machine_type = "n1-standard-1"
-    disk_size_gb = 100
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/compute",
-      "https://www.googleapis.com/auth/devstorage.read_only",
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-    ]
-  }
 }
