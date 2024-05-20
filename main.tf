8582be0a576dea0a2c701f0d66cadc73d4406217
Sneghapriya <102730861+Sneghapriya@users.noreply.github.com>
2024-05-20 12:40:59 +0530

    update

--- main.tf
+++ main.tf
@@ -16,19 +16,24 @@ resource "google_container_cluster" "primary" {
   remove_default_node_pool = true
   initial_node_count       = 1
 
-  networking_mode = "k8s_service"
+  networking_mode = "k8s_ip_aliases_with_ipsec"
 
+  workload_identity_config {
+ workload_pool = "sampleproject.svc.id.goog"
+  }
 }
 
 resource "google_container_node_pool" "pool1" {
   name       = "pool-1"
-  cluster    = google_container_cluster.primary.name
   location  = "us-central1"
- project   = "sampleproject"
+  project   = "sampleproject"
+  cluster   = google_container_cluster.primary.name
 
   node_config {
     machine_type = "n1-standard-1"
+    disk_size_gb = 100
   }
+
   autoscaling {
     enabled = false
   }
@@ -36,13 +41,15 @@ resource "google_container_node_pool" "pool1" {
 
 resource "google_container_node_pool" "pool2" {
   name       = "pool-2"
-  cluster    = google_container_cluster.primary.name
   location  = "us-central1"
- project   = "sampleproject"
+  project   = "sampleproject"
+  cluster   = google_container_cluster.primary.name
 
   node_config {
     machine_type = "n1-standard-2"
+    disk_size_gb = 100
   }
+
   autoscaling {
     enabled = false
   }
