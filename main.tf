a95a7ff5d170c1f8e8d4e5391841fedc9e71fa37
Sneghapriya <102730861+Sneghapriya@users.noreply.github.com>
2024-05-20 15:50:28 +0530

    update

--- main.tf
+++ main.tf
@@ -22,32 +22,56 @@ resource "google_container_cluster" "primary" {
   }
 }
 
-resource "google_container_node_pool" "pool_1" {
+resource "google_container_node_pool" "pool1" {
   name       = "pool-1"
   location  = "us-central1"
   project   = "sampleproject"
   cluster   = google_container_cluster.primary.name
-
-  node_config {
-    machine_type = "n1-standard-1"
-  }
+  node_count = 1
 
   autoscaling {
     enabled = false
   }
+  management {
+    auto_repair  = true
+    auto_upgrade = true
+  }
+
+  node_config {
+    machine_type = "n1-standard-1"
+    disk_size_gb = 100
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/compute",
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+  }
 }
 
-resource "google_container_node_pool" "pool_2" {
+resource "google_container_node_pool" "pool2" {
   name       = "pool-2"
   location  = "us-central1"
   project   = "sampleproject"
   cluster   = google_container_cluster.primary.name
-
-  node_config {
-    machine_type = "n1-standard-2"
-  }
+  node_count = 1
 
   autoscaling {
     enabled = false
+  }
+  management {
+    auto_repair  = true
+    auto_upgrade = true
+  }
+
+  node_config {
+    machine_type = "n1-standard-1"
+    disk_size_gb = 100
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/compute",
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
   }
 }
