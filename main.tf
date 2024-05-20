4db6e512a36a602911f847a7e8f100d4666c5c87
Sneghapriya <102730861+Sneghapriya@users.noreply.github.com>
2024-05-20 12:30:26 +0530

    update

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 4.0"
+      version = "~> 4.0.0"
     }
   }
 }
@@ -12,56 +12,31 @@ resource "google_container_cluster" "primary" {
   name     = "test-cluster"
   location = "us-central1"
  project  = "sampleproject"
-
   remove_default_node_pool = true
   initial_node_count       = 1
-
-  networking_mode = "k8s_service"
-
+ networking_mode = "VPC_NATIVE"
   ip_allocation_policy {
   }
 }
 
-resource "google_container_node_pool" "pool1" {
+resource "google_container_node_pool" "pool_1" {
   name       = "pool-1"
   location  = "us-central1"
   project   = "sampleproject"
   cluster   = google_container_cluster.primary.name
   node_count = 1
-
   autoscaling {
     enabled = false
   }
-
-  management {
-    auto_repair  = true
-    auto_upgrade = true
-  }
-
-  node_config {
-    machine_type = "n1-standard-1"
-    disk_size_gb = 100
-  }
 }
 
-resource "google_container_node_pool" "pool2" {
+resource "google_container_node_pool" "pool_2" {
   name       = "pool-2"
   location  = "us-central1"
   project   = "sampleproject"
   cluster   = google_container_cluster.primary.name
   node_count = 1
-
   autoscaling {
     enabled = false
-  }
-
-  management {
-    auto_repair  = true
-    auto_upgrade = true
-  }
-
-  node_config {
-    machine_type = "n1-standard-1"
-    disk_size_gb = 100
   }
 }
