b0a4d4ccbc72f0ea99c94dfa2a0aee29d2f064a3
Sneghapriya <102730861+Sneghapriya@users.noreply.github.com>
2024-05-20 18:06:21 +0530

    update

--- main.tf
+++ main.tf
@@ -3,77 +3,69 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 4.0"
+      version = "~> 4.0.0"
     }
   }
 }
 
 resource "google_container_cluster" "primary" {
   name     = "test-cluster"
-  location = "us-central1-a"
-  project  = "sampleproject"
+  location = "us-central1"
+ project  = "sampleproject"
 
   remove_default_node_pool = true
-  initial_node_count       = 1
-
-  networking_mode = "k8s_pod_networking"
-
+  initial_node_count = 1
+ networking_mode = "k8s-ip-alias"
   ip_allocation_policy {
   }
 }
 
-resource "google_container_node_pool" "primary_nodes" {
-  name       = "primary-node-pool"
-  location  = "us-central1-a"
+resource "google_container_node_pool" "pool1" {
+  name       = "pool-1"
+  location  = "us-central1"
   project   = "sampleproject"
   cluster   = google_container_cluster.primary.name
-
   node_count = 1
 
   autoscaling {
     enabled = false
   }
-
   management {
     auto_repair  = true
     auto_upgrade = true
   }
-
   node_config {
     machine_type = "n1-standard-1"
     disk_size_gb = 100
     oauth_scopes = [
       "https://www.googleapis.com/auth/compute",
-      "https://www.googleapis.com/auth/devstorage.read_write",
+      "https://www.googleapis.com/auth/devstorage.read_only",
       "https://www.googleapis.com/auth/logging.write",
       "https://www.googleapis.com/auth/monitoring",
     ]
   }
 }
 
-resource "google_container_node_pool" "secondary_nodes" {
-  name       = "secondary-node-pool"
-  location  = "us-central1-a"
+resource "google_container_node_pool" "pool2" {
+  name       = "pool-2"
+  location  = "us-central1"
   project   = "sampleproject"
   cluster   = google_container_cluster.primary.name
-
   node_count = 1
 
   autoscaling {
     enabled = false
   }
-
   management {
     auto_repair  = true
     auto_upgrade = true
   }
-
   node_config {
     machine_type = "n1-standard-1"
     disk_size_gb = 100
     oauth_scopes = [
       "https://www.googleapis.com/auth/compute",
-      "https://www.googleapis.com/auth/devstorage.read_write",
+      "https://www.googleapis.com/auth/devstorage.read_only",
       "https://www.googleapis.com/auth/logging.write",
       "https://www.googleapis.com/auth/monitoring",
     ]
