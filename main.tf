da7f396e799e406dcfaefcc2e2793a47b9d93733
Sneghapriya <102730861+Sneghapriya@users.noreply.github.com>
2024-05-20 16:39:43 +0530

    update

--- main.tf
+++ main.tf
@@ -13,23 +13,28 @@ resource "google_container_cluster" "primary" {
   location = "us-central1"
  project  = "sampleproject"
 
-  remove_default_node_pool = true
-  initial_node_count       = 1
-
-  networking_mode = "k8s_service"
+  networking_mode = "VPC_NATIVE"
+  initial_node_count = 1
 
   ip_allocation_policy {
+  }
+
+  remove_default_node_pool = true
+
+  release_channel {
+    channel = "REGULAR"
   }
 }
 
 resource "google_container_node_pool" "pool1" {
   name       = "pool-1"
+  cluster    = google_container_cluster.primary.name
   location  = "us-central1"
   project   = "sampleproject"
-  cluster   = google_container_cluster.primary.name
-  node_count = 1
 
-  autoscaling {}
+  autoscaling {
+    enabled = false
+  }
 
   management {
     auto_repair  = true
@@ -37,25 +42,21 @@ resource "google_container_node_pool" "pool1" {
   }
 
   node_config {
-    machine_type = "n1-standard-1"
+    machine_type = "e2-medium"
     disk_size_gb = 100
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/compute",
-      "https://www.googleapis.com/auth/devstorage.read_only",
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-    ]
+    disk_type    = "pd-standard"
   }
 }
 
 resource "google_container_node_pool" "pool2" {
   name       = "pool-2"
+  cluster    = google_container_cluster.primary.name
   location  = "us-central1"
   project   = "sampleproject"
-  cluster   = google_container_cluster.primary.name
-  node_count = 1
 
-  autoscaling {}
+  autoscaling {
+    enabled = false
+  }
 
   management {
     auto_repair  = true
@@ -63,13 +64,8 @@ resource "google_container_node_pool" "pool2" {
   }
 
   node_config {
-    machine_type = "n1-standard-1"
+    machine_type = "e2-medium"
     disk_size_gb = 100
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/compute",
-      "https://www.googleapis.com/auth/devstorage.read_only",
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-    ]
+    disk_type    = "pd-standard"
   }
 }
