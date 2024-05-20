35d8290483b26e337ded4ab7b43467aba22b2411
Sneghapriya <102730861+Sneghapriya@users.noreply.github.com>
2024-05-20 21:32:15 +0530

    update

--- main.tf
+++ main.tf
@@ -10,71 +10,36 @@ terraform {
 
 resource "google_container_cluster" "primary" {
   name     = "test-cluster"
-  location = "us-central1"
- project  = "sampleproject"
-
-  remove_default_node_pool = true
-  initial_node_count       = 1
+  location = "us-central1-a"
+  project  = "sampleproject"
 
   networking_mode = "VPC_NATIVE"
+  initial_node_count = 1
 
-  workload_identity_config {
- workload_pool = "sampleproject.svc.id.goog"
-  }
-}
-
-resource "google_container_node_pool" "pool1" {
-  name       = "pool-1"
-  location  = "us-central1"
-  project   = "sampleproject"
-  cluster   = google_container_cluster.primary.name
-  node_count = 1
-
-  autoscaling {
-    enabled = false
+  ip_allocation_policy {
   }
 
-  management {
-    auto_repair  = true
-    auto_upgrade = true
+  remove_default_node_pool = true
+
+  node_pool {
+    name       = "default-pool"
+    node_count = 1
+    autoscaling = false
+
+    management {
+      auto_repair  = true
+      auto_upgrade = true
+    }
   }
 
-  node_config {
-    machine_type = "n1-standard-1"
-    disk_size_gb = 100
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/compute",
-      "https://www.googleapis.com/auth/devstorage.read_only",
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-    ]
-  }
-}
+  node_pool {
+    name       = "secondary-pool"
+    node_count = 1
+    autoscaling = false
 
-resource "google_container_node_pool" "pool2" {
-  name       = "pool-2"
-  location  = "us-central1"
-  project   = "sampleproject"
-  cluster   = google_container_cluster.primary.name
-  node_count = 1
-
-  autoscaling {
-    enabled = false
-  }
-
-  management {
-    auto_repair  = true
-    auto_upgrade = true
-  }
-
-  node_config {
-    machine_type = "n1-standard-1"
-    disk_size_gb = 100
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/compute",
-      "https://www.googleapis.com/auth/devstorage.read_only",
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-    ]
+    management {
+      auto_repair  = true
+      auto_upgrade = true
+    }
   }
 }
