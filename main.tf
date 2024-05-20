59a56264ee17430e9aa536c7736a7ee3e32e6810
Sneghapriya <102730861+Sneghapriya@users.noreply.github.com>
2024-05-20 16:38:54 +0530

    update

--- main.tf
+++ main.tf
@@ -18,8 +18,7 @@ resource "google_container_cluster" "primary" {
 
   networking_mode = "k8s_service"
 
-  workload_identity_config {
- workload_pool = "sampleproject.svc.id.goog"
+  ip_allocation_policy {
   }
 }
 
@@ -28,14 +27,24 @@ resource "google_container_node_pool" "pool1" {
   location  = "us-central1"
   project   = "sampleproject"
   cluster   = google_container_cluster.primary.name
+  node_count = 1
+
+  autoscaling {}
+
+  management {
+    auto_repair  = true
+    auto_upgrade = true
+  }
 
   node_config {
     machine_type = "n1-standard-1"
     disk_size_gb = 100
-  }
-
-  autoscaling {
-    enabled = false
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/compute",
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
   }
 }
 
@@ -44,13 +53,23 @@ resource "google_container_node_pool" "pool2" {
   location  = "us-central1"
   project   = "sampleproject"
   cluster   = google_container_cluster.primary.name
+  node_count = 1
 
-  node_config {
-    machine_type = "n1-standard-2"
-    disk_size_gb = 200
+  autoscaling {}
+
+  management {
+    auto_repair  = true
+    auto_upgrade = true
   }
 
-  autoscaling {
-    enabled = false
+  node_config {
+    machine_type = "n1-standard-1"
+    disk_size_gb = 100
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/compute",
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
   }
 }
