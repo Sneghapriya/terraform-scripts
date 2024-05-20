7c70e5245f70abb1083ecdfb896766c693a13de1
Sneghapriya <102730861+Sneghapriya@users.noreply.github.com>
2024-05-20 15:52:05 +0530

    update

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ terraform {
 
 resource "google_container_cluster" "primary" {
   name     = "test-cluster"
-  location = "us-central1-a"
+  location = "us-central1"
  project  = "sampleproject"
 
   remove_default_node_pool = true
@@ -18,26 +18,18 @@ resource "google_container_cluster" "primary" {
 
   networking_mode = "VPC_NATIVE"
 
-  workload_identity_config {
- workload_pool = "sampleproject.svc.id.goog"
+  ip_allocation_policy {
   }
 }
 
 resource "google_container_node_pool" "pool1" {
   name       = "pool-1"
-  location  = "us-central1-a"
+  location  = "us-central1"
   project   = "sampleproject"
   cluster   = google_container_cluster.primary.name
 
   node_config {
     machine_type = "n1-standard-1"
-    disk_size_gb = 100
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/compute",
-      "https://www.googleapis.com/auth/devstorage.read_only",
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-    ]
   }
 
   autoscaling {
@@ -47,19 +39,12 @@ resource "google_container_node_pool" "pool1" {
 
 resource "google_container_node_pool" "pool2" {
   name       = "pool-2"
-  location  = "us-central1-a"
+  location  = "us-central1"
   project   = "sampleproject"
   cluster   = google_container_cluster.primary.name
 
   node_config {
     machine_type = "n1-standard-2"
-    disk_size_gb = 100
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/compute",
-      "https://www.googleapis.com/auth/devstorage.read_only",
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-    ]
   }
 
   autoscaling {
