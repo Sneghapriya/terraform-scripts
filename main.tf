b4200883c5d5e5de6cdf621e481152f730278402
Sneghapriya <102730861+Sneghapriya@users.noreply.github.com>
2024-05-20 15:49:45 +0530

    update

--- main.tf
+++ main.tf
@@ -8,70 +8,36 @@ terraform {
   }
 }
 
-provider "google" {
-  project = "sampleproject"
-}
-
 resource "google_container_cluster" "primary" {
   name     = "test-cluster"
   location = "us-central1"
-
-  networking_mode = "VPC_NATIVE"
-  initial_node_count = 1
-
-  ip_allocation_policy {
-  }
+ project  = "sampleproject"
 
   remove_default_node_pool = true
 
-}
+  network = "default"
 
-resource "google_container_node_pool" "pool1" {
-  name       = "pool-1"
-  location  = "us-central1"
-  cluster   = google_container_cluster.primary.name
-  node_count = 1
+  node_pool {
+    name       = "primary-node-pool"
+    node_count = 1
 
-  autoscaling {}
+    autoscaling = false
 
-  management {
-    auto_repair  = true
-    auto_upgrade = true
+    management {
+      auto_repair  = true
+      auto_upgrade = true
+    }
   }
 
-  node_config {
-    machine_type = "n1-standard-1"
-    disk_size_gb = 100
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/compute",
-      "https://www.googleapis.com/auth/devstorage.read_only",
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-    ]
-  }
-}
+  node_pool {
+    name       = "secondary-node-pool"
+    node_count = 1
 
-resource "google_container_node_pool" "pool2" {
-  name       = "pool-2"
-  location  = "us-central1"
-  cluster   = google_container_cluster.primary.name
-  node_count = 1
+    autoscaling = false
 
-  autoscaling {}
-
-  management {
-    auto_repair  = true
-    auto_upgrade = true
-  }
-
-  node_config {
-    machine_type = "n1-standard-1"
-    disk_size_gb = 100
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/compute",
-      "https://www.googleapis.com/auth/devstorage.read_only",
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-    ]
+    management {
+      auto_repair  = true
+      auto_upgrade = true
+    }
   }
 }
