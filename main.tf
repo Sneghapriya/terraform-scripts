3fada95164165df1e472c1ee8efda3d1c9009af7
Sneghapriya <102730861+Sneghapriya@users.noreply.github.com>
2024-05-20 15:37:33 +0530

    update

--- main.tf
+++ main.tf
@@ -10,43 +10,32 @@ terraform {
 
 resource "google_container_cluster" "primary" {
   name     = "test-cluster"
-  location = "us-central1-a"
+  location = "us-central1"
  project  = "sampleproject"
-
   remove_default_node_pool = true
-  initial_node_count       = 1
-
-  networking_mode = "VPC_NATIVE"
-
-  ip_allocation_policy {
-  }
+  initial_node_count = 1
+ networking_mode = "VPC_NATIVE"
+ ip_allocation_policy {
+ }
 }
 
-resource "google_container_node_pool" "pool1" {
+resource "google_container_node_pool" "pool_1" {
   name       = "pool-1"
-  location  = "us-central1-a"
+  location  = "us-central1"
   project   = "sampleproject"
   cluster   = google_container_cluster.primary.name
-
-  node_config {
-    machine_type = "n1-standard-1"
-  }
-
+  node_count = 1
   autoscaling {
     enabled = false
   }
 }
 
-resource "google_container_node_pool" "pool2" {
+resource "google_container_node_pool" "pool_2" {
   name       = "pool-2"
-  location  = "us-central1-a"
+  location  = "us-central1"
   project   = "sampleproject"
   cluster   = google_container_cluster.primary.name
-
-  node_config {
-    machine_type = "n1-standard-2"
-  }
-
+  node_count = 1
   autoscaling {
     enabled = false
   }
