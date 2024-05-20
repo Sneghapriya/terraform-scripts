7ca4c86a2e9ff41c2a3664332721521acf6b39ef
Sneghapriya <102730861+Sneghapriya@users.noreply.github.com>
2024-05-20 15:38:03 +0530

    update

--- main.tf
+++ main.tf
@@ -10,30 +10,28 @@ terraform {
 
 resource "google_container_cluster" "primary" {
   name     = "test-cluster"
-  location = "us-central1-a"
- project  = "sampleproject"
-
-  networking_mode = "VPC_NATIVE"
-  ip_allocation_policy {
-  }
+  location = "us-central1"
+  project  = "sampleproject"
 
   remove_default_node_pool = true
+  initial_node_count       = 1
 
-  release_channel {
-    channel = "REGULAR"
+  networking_mode = "k8s_ipo_alloc"
+
+  ip_allocation_policy {
   }
 }
 
 resource "google_container_node_pool" "pool1" {
   name       = "pool-1"
-  location  = "us-central1-a"
+  location  = "us-central1"
   project   = "sampleproject"
   cluster   = google_container_cluster.primary.name
-
   node_count = 1
 
-  autoscaling {}
-
+  autoscaling {
+    enabled = false
+  }
   management {
     auto_repair  = true
     auto_upgrade = true
@@ -42,8 +40,6 @@ resource "google_container_node_pool" "pool1" {
   node_config {
     machine_type = "n1-standard-1"
     disk_size_gb = 100
-    disk_type    = "pd-standard"
-
     oauth_scopes = [
       "https://www.googleapis.com/auth/compute",
       "https://www.googleapis.com/auth/devstorage.read_only",
@@ -55,14 +51,14 @@ resource "google_container_node_pool" "pool1" {
 
 resource "google_container_node_pool" "pool2" {
   name       = "pool-2"
-  location  = "us-central1-a"
+  location  = "us-central1"
   project   = "sampleproject"
   cluster   = google_container_cluster.primary.name
-
   node_count = 1
 
-  autoscaling {}
-
+  autoscaling {
+    enabled = false
+  }
   management {
     auto_repair  = true
     auto_upgrade = true
@@ -71,8 +67,6 @@ resource "google_container_node_pool" "pool2" {
   node_config {
     machine_type = "n1-standard-1"
     disk_size_gb = 100
-    disk_type    = "pd-standard"
-
     oauth_scopes = [
       "https://www.googleapis.com/auth/compute",
       "https://www.googleapis.com/auth/devstorage.read_only",
